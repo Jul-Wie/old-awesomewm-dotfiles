7e63c9e80ff877ca088c59bb5c47fbfebc5ac585
Jul-Wie <juulwie@protonmail.com>
2023-12-25 13:21:40 +0100

    added awesome and alacritty configs

---
+++ .config/awesome/rc.lua
@@ -0,0 +1,32 @@
+-----------------------------------------------------------------------------------------
+--  █████╗ ██╗    ██╗███████╗███████╗ ██████╗ ███╗   ███╗███████╗██╗    ██╗███╗   ███╗ --
+-- ██╔══██╗██║    ██║██╔════╝██╔════╝██╔═══██╗████╗ ████║██╔════╝██║    ██║████╗ ████║ --
+-- ███████║██║ █╗ ██║█████╗  ███████╗██║   ██║██╔████╔██║█████╗  ██║ █╗ ██║██╔████╔██║ --
+-- ██╔══██║██║███╗██║██╔══╝  ╚════██║██║   ██║██║╚██╔╝██║██╔══╝  ██║███╗██║██║╚██╔╝██║ --
+-- ██║  ██║╚███╔███╔╝███████╗███████║╚██████╔╝██║ ╚═╝ ██║███████╗╚███╔███╔╝██║ ╚═╝ ██║ --
+-- ╚═╝  ╚═╝ ╚══╝╚══╝ ╚══════╝╚══════╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝ ╚══╝╚══╝ ╚═╝     ╚═╝ --
+-----------------------------------------------------------------------------------------
+-- Initialising, order is important!
+require("src.theme.user_variables")
+require("src.theme.init")
+require("src.core.error_handling")
+require("src.core.signals")
+require("src.core.notifications")
+require("src.core.rules")
+require("mappings.global_buttons")
+require("mappings.bind_to_tags")
+require("crylia_bar.init")
+local awful = require("awful")
+
+autorun = true
+autorunApps =
+{
+   "picom",
+   "sh /home/jul/scripts/drive.sh",
+}
+if autorun then
+   for app = 1, #autorunApps do
+       awful.util.spawn(autorunApps[app])
+   end
+end
+awful.util.spawn("nm-applet")
